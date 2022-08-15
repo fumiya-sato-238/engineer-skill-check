77b46918d9899ac66729c274a9cbb83bd8089be4
{fumiya-sato-238} <{megaonigouri@gmail.com}>
2022-08-15 20:54:49 +0900

    投稿機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,20 @@
 class ArticlesController < ApplicationController
   def new
+    @articles = Article.new
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save
+    redirect_to '/'
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content, :author)
   end
 end
